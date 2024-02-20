30cb82a1e48a2c8a40bb0e45e4592d9490c16411
Paws <131801287+paws1234@users.noreply.github.com>
2024-02-20 14:01:21 +0800

    Update index.php

--- lms/api/index.php
+++ lms/api/index.php
@@ -1,19 +1,2 @@
 <?php
-
-// Load the Laravel application
-require __DIR__ . '/../bootstrap/app.php';
-
-// Import the User model namespace outside of the function declaration
-use App\Models\User;
-
-// Set up Laravel router
-$app->router->get('/api/users', function () use ($app) {
-    // Retrieve all users using the User model
-    $users = User::all();
-
-    // Return the users as a JSON response
-    return response()->json(['users' => $users]);
-});
-
-// Run the Laravel application
-$app->run();
+require __DIR__ . '/../public/index.php';
